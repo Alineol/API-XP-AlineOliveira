b5016f6ed230cff85b096020f704d4158e616b38
Aline <allinne.oliveira.ol@gmail.com>
2022-07-17 19:23:55 -0300

    :sparkles: (routes/index.ts) feat: adiciona rota de ativos

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import contaRoutes from './contaRoutes';
 import userRoutes from './userRoutes';
+import ativoRoutes from './ativoRoutes';
 
 const route = Router();
 
 route.use('/conta', contaRoutes);
+route.use('/ativos', ativoRoutes);
 route.use('/', userRoutes);
 
 export default route;
