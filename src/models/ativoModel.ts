61cac4b5dbb48460db91dd0e3e4fb4a6b0740049
Aline <allinne.oliveira.ol@gmail.com>
2022-07-17 19:59:55 -0300

    :sparkles: (ativoModel.ts) feat: cria função getByCodCliente

--- src/models/ativoModel.ts
+++ src/models/ativoModel.ts
@@ -1,12 +1,21 @@
 import connection from './connection';
 import IAtivo from '../interfaces/IAtivo';
+import IAtivoUsuario from '../interfaces/IAtivoUsuario';
 
 const getByCodAtivo = async (cod: number): Promise<IAtivo[]> => {
   const query = 'SELECT * FROM XPCorretora.AtivosCorretora WHERE CodAtivo = ?;';
+  // ! mudar o nome dessa variavel
   const [Account] = await connection.execute(query, [cod]);
   return Account as IAtivo[];
 };
 
+const getByCodCliente = async (cod: number): Promise<IAtivoUsuario[]> => {
+  const query = 'SELECT * FROM XPCorretora.AtivosUsuarios WHERE CodCliente = ?;';
+  const [ativos] = await connection.execute(query, [cod]);
+  return ativos as IAtivoUsuario[];
+};
+
 export default {
   getByCodAtivo,
+  getByCodCliente,
 };
