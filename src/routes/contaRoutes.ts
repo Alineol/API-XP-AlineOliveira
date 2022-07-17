ae8f131f48f9c6a07d010ba33d588a9dcb992307
Aline <allinne.oliveira.ol@gmail.com>
2022-07-17 17:06:59 -0300

    :sparkles: (contaRoutes.ts) feat: adicina rota post/conta/deposito

--- src/routes/contaRoutes.ts
+++ src/routes/contaRoutes.ts
@@ -9,4 +9,6 @@ route.get('/:CodCliente', userMid.validatetoken, contaController.getByCodCliente
 
 route.post('/saque', contaMid.validateBody, contaController.getMoney);
 
+route.post('/deposito', contaMid.validateBody, contaController.putMoney);
+
 export default route;
