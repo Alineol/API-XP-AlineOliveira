45de9843aaaabf631e6b5b5b7b4c8cff03efc17a
Aline <allinne.oliveira.ol@gmail.com>
2022-07-17 13:46:56 -0300

    :sparkles:  feat: cria rota de usuario

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import contaRoutes from './contaRoutes'
+import contaRoutes from './contaRoutes';
+import userRoutes from './userRoutes';
 
 const route = Router();
 
-route.use('/conta', contaRoutes)
-
+route.use('/conta', contaRoutes);
+route.use('/', userRoutes);
 
 export default route;
