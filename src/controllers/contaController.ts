eddb97f906837c9eef5b951ebd0384f8984eb67a
Aline <allinne.oliveira.ol@gmail.com>
2022-07-17 14:54:42 -0300

    :lock: (contaService.ts) securty: verificação de usuario para saldo
    só pode ver o saldo se estiver logado e através do token é feita uma verificação para ver se o solicitante é dono da conta

--- src/controllers/contaController.ts
+++ src/controllers/contaController.ts
@@ -4,8 +4,9 @@ import contaService from '../services/contaService';
 
 const getByCodCliente = async (req: Request, res: Response): Promise<Response> => {
   const { CodCliente } = req.params;
-  const account = await contaService.getByCodCliente(Number(CodCliente));
-
+  const { authorization } = req.headers;
+  const account = await contaService.getByCodCliente(Number(CodCliente), authorization as string);
+  if (account === '') { return res.status(401).json({ message: 'Invalid token' }); }
   return res.status(200).json(account);
 };
 
--- src/services/contaService.ts
+++ src/services/contaService.ts
@@ -1,13 +1,28 @@
-import contaModel from "../models/contaModel";
+import contaModel from '../models/contaModel';
+import userModel from '../models/userModel';
+import jwt from '../jwt';
+import IUser from '../interfaces/IUser';
+import IAccount from '../interfaces/IAccount';
 
-const getByCodCliente = async(cod: number) => {
+const checkAuthorization = async (token: string, cod: number): Promise<boolean> => {
+  const tokenEmail = await jwt.checkToken(token) as IUser;
+  const { email } = tokenEmail;
+  const [codCliente] = await userModel.getUserByEmail(email);
+  return codCliente.CodCliente === cod;
+};
+
+const getByCodCliente = async (cod: number, token: string):Promise<IAccount | string> => {
   const [account] = await contaModel.getByCodCliente(cod);
- return {
-  CodCliente: account.CodCliente,
-  Valor: Number(account.Valor)
-}
-}
+  const authorization = await checkAuthorization(token, cod);
+  if (!authorization) {
+    return '';
+  }
+  return {
+    CodCliente: account.CodCliente,
+    Valor: Number(account.Valor),
+  };
+};
 
 export default {
   getByCodCliente,
-}
+};
