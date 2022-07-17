8d3643b4c2c9d5f1a956fdf1b43755896938d5de
Aline <allinne.oliveira.ol@gmail.com>
2022-07-17 14:57:12 -0300

    :sparkles: (contaRoutes.ts) adiciona verificação de token

--- src/routes/contaRoutes.ts
+++ src/routes/contaRoutes.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import contaController from '../controllers/contaController';
+import userMid from '../middlewares/userMiddlewares';
 
 const route = Router();
 
-route.get('/:CodCliente', contaController.getByCodCliente)
+route.get('/:CodCliente', userMid.validatetoken, contaController.getByCodCliente);
 
 export default route;
