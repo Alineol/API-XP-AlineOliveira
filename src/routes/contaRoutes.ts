4f8f996a2670f2a83cec4295b9def8bc87d59800
Aline <allinne.oliveira.ol@gmail.com>
2022-07-17 16:52:19 -0300

    :sparkles: (contaRoutes.ts) feat: adiciona rota de post.conta/saque

--- src/routes/contaRoutes.ts
+++ src/routes/contaRoutes.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 import contaController from '../controllers/contaController';
 import userMid from '../middlewares/userMiddlewares';
+import contaMid from '../middlewares/accounMiddlewares';
 
 const route = Router();
 
 route.get('/:CodCliente', userMid.validatetoken, contaController.getByCodCliente);
+
+route.post('/saque', contaMid.validateBody, contaController.getMoney);
 
 export default route;
