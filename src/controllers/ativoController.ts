77ac216b03adffcc0498bb2ca9ec158d103896d5
Aline <allinne.oliveira.ol@gmail.com>
2022-07-18 11:53:19 -0300

    feat(ativocontroller.ts): adiciona função getByCodCliente

--- src/controllers/ativoController.ts
+++ src/controllers/ativoController.ts
@@ -9,6 +9,15 @@ const getByCodAtivo = async (req: Request, res: Response): Promise<Response> => 
   return res.status(200).json(ativo);
 };
 
+const getByCodCliente = async (req: Request, res: Response): Promise<Response> => {
+  const { CodCliente } = req.params;
+  const { authorization } = req.headers;
+  const ativos = await ativoService.getByCodCliente(Number(CodCliente), authorization as string);
+  if (ativos === '') return res.status(401).json({ message: 'Token invalido, sem autorização' }); 
+  return res.status(200).json(ativos);
+};
+
 export default {
   getByCodAtivo,
+  getByCodCliente,
 };
