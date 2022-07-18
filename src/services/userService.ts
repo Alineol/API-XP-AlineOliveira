abaad2a0b49ebdb3bbc3d2ee0201a57000873b84
Aline <allinne.oliveira.ol@gmail.com>
2022-07-18 12:15:49 -0300

    ♻️ refactor(userservice.ts): adiciona função checkAturhorization
    essa função acessa a model de user então se encaixa melhor na service de user

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,7 +1,14 @@
 // import IUser from '../interfaces/IUser';
 import userModel from '../models/userModel';
 import jwt from '../jwt';
-// import IUser from '../interfaces/IUser';
+import IUser from '../interfaces/IUser';
+
+const checkAuthorization = async (token: string, cod: number): Promise<boolean> => {
+  const tokenEmail = await jwt.checkToken(token) as IUser;
+  const { email } = tokenEmail;
+  const [codCliente] = await userModel.getUserByEmail(email);
+  return codCliente.CodCliente === cod;
+};
 
 const login = async (email: string, senha: string): Promise<string> => {
   const [user] = await userModel.login(email, senha);
@@ -10,4 +17,4 @@ const login = async (email: string, senha: string): Promise<string> => {
   return token;
 };
 
-export default { login };
+export default { login, checkAuthorization };
