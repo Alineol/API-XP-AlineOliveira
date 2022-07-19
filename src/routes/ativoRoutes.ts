d6c4594b6d2d7ec5e271c919a9226a04aebd0cd5
Aline <allinne.oliveira.ol@gmail.com>
2022-07-19 18:10:56 -0300

    ♻️ refactor(ativoroute.ts): remove routa '/cliente/:CodCliente
    sera reaolocada em userRouter.ts

--- src/routes/ativoRoutes.ts
+++ src/routes/ativoRoutes.ts
@@ -1,4 +1,4 @@
-import { Router, Request, Response } from 'express';
+import { Router } from 'express';
 import ativoController from '../controllers/ativoController';
 import userMid from '../middlewares/userMiddlewares';
 import mid from '../middlewares/ativoMiddleware';
@@ -6,23 +6,10 @@ import mid from '../middlewares/ativoMiddleware';
 const route = Router();
 
 route.get(
-  '/corretora/:CodAtivo', 
+  '/:CodAtivo', 
   userMid.validatetoken,
   mid.validateAtivosCorretoraParams, 
   ativoController.getAtivosCorretoraByCodAtivo,
 );
-
-route.get(
-  '/cliente/:CodCliente',
-  userMid.validatetoken,
-  mid.validateAtivosClienteParams,
-  ativoController.getAtivosUsuarioByCodCliente,
-);
-
-route.get('/corretora', (req: Request, res: Response) => res.status(400).json({
-  message: 'URL incompleta, necesssita de um parâmetro' }));
-
-route.get('/cliente', (req: Request, res: Response) => res.status(400).json({
-  message: 'URL incompleta, necesssita de um parâmetro' }));
 
 export default route;
