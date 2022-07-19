2a0a3fc434c045cd056b03dbcaaff57643da0a31
Aline <allinne.oliveira.ol@gmail.com>
2022-07-19 18:35:05 -0300

    ✨ feat(routes/index.ts): adiciona rota de login e remove rota de conta

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
-import contaRoutes from './contaRoutes';
 import userRoutes from './userRoutes';
 import ativoRoutes from './ativoRoutes';
+import loginRoute from './loginRoute';
+import userMid from '../middlewares/userMiddlewares';
 
 const route = Router();
 
-route.use('/conta', contaRoutes);
 route.use('/ativos', ativoRoutes);
-route.use('/', userRoutes);
+route.use('/cliente', userMid.validatetoken, userRoutes);
+route.use('/login', loginRoute);
 
 export default route;
