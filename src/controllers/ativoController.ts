558e36f4b3cac2e1adde0d59d096111a33268996
Aline <allinne.oliveira.ol@gmail.com>
2022-07-19 17:54:26 -0300

    ♻️ refactor(ativocontroller.ts): remove função getAtivosUsuarioByCodCliente
    a função foi realocada no arquivo ativoUsuarioController

--- src/controllers/ativoController.ts
+++ src/controllers/ativoController.ts
@@ -9,16 +9,6 @@ const getAtivosCorretoraByCodAtivo = async (req: Request, res: Response): Promis
   return res.status(200).json(ativo);
 };
 
-const getAtivosUsuarioByCodCliente = async (req: Request, res: Response): Promise<Response> => {
-  const { CodCliente } = req.params;
-  const { authorization } = req.headers;
-  const ativos = await ativoService
-    .getAtivosUsuarioByCodCliente(Number(CodCliente), authorization as string);
-  if (ativos === '') return res.status(401).json({ message: 'Token invalido, sem autorização' }); 
-  return res.status(200).json(ativos);
-};
-
 export default {
   getAtivosCorretoraByCodAtivo,
-  getAtivosUsuarioByCodCliente,
 };
