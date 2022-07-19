8eec9f72c56bcd5fb312a87f1387125f3fd0c51f
Aline <allinne.oliveira.ol@gmail.com>
2022-07-19 18:12:14 -0300

    ✨ feat(userroutes.tc): adicona rota '/ativos/:CodCliente e remove rota de login

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,9 +1,29 @@
 import { Router } from 'express';
-import userController from '../controllers/userController';
-import mid from '../middlewares/userMiddlewares';
+// import userController from '../controllers/userController';
+import contaController from '../controllers/contaController';
+import accountMid from '../middlewares/accounMiddlewares';
+import mid from '../middlewares/ativoMiddleware';
+import ativoUsuarioController from '../controllers/ativoUsuarioController';
 
 const route = Router();
 
-route.post('/login', mid.validateLoginBody, userController.login);
+route.get('/conta/:CodCliente', contaController.getByCodCliente);
 
+route.post(
+  '/conta/saque', 
+  accountMid.validateBody, 
+  contaController.getMoney,
+);
+
+route.post(
+  '/conta/deposito', 
+  accountMid.validateBody, 
+  contaController.putMoney,
+);
+
+route.get(
+  '/ativos/:CodCliente',
+  mid.validateAtivosClienteParams,
+  ativoUsuarioController.getAtivosUsuarioByCodCliente,
+);
 export default route;
