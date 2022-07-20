d59df117af4a5eab28f28e4950809df8a54b8c64
Aline <allinne.oliveira.ol@gmail.com>
2022-07-20 10:06:12 -0300

    ✨ feat(ativousuariomodel.ts): adiciona 2 novas funções na model
    uma função serve para decrementar a quantidade de um determinado ativo do usuario, a outra serve
    para buscar o ativo de acordo com o codAtivo e codUsuario

--- src/models/ativoUsuarioModel.ts
+++ src/models/ativoUsuarioModel.ts
@@ -1,36 +1,37 @@
-// import { ResultSetHeader } from 'mysql2';
+import { ResultSetHeader } from 'mysql2';
 import connection from './connection';
 import IAtivoUsuario from '../interfaces/IAtivoUsuario';
 
-// const cliente = 'XPCorretora.AtivosUsuarios';
+const bd = 'XPCorretora.AtivosUsuarios';
 
 const getAtivosUsuarioByCodCliente = async (cod: number): Promise<IAtivoUsuario[]> => {
+  // !mudar essa query depois 
   const query = 'SELECT * FROM XPCorretora.AtivosUsuarios WHERE CodCliente = ?;';
   const [ativos] = await connection.execute(query, [cod]);
   return ativos as IAtivoUsuario[];
 };
 
-// const getAtivosUsuarioByCodClienteAndCodAtivo = async (
-//   codAtivo:number,
-//   CodCliente: number,
-// ): Promise<IAtivoUsuario[]> => {
-//   const query = 'SELECT * FROM XPCorretora.AtivosUsuarios WHERE CodCliente = ? AND CodAtivo = ?;';
-//   const [ativos] = await connection.execute(query, [CodCliente, codAtivo]);
-//   return ativos as IAtivoUsuario[];
-// };
+const getAtivosUsuarioByCodClienteAndCodAtivo = async (
+  codAtivo:number,
+  CodCliente: number,
+): Promise<IAtivoUsuario[]> => {
+  const query = `SELECT * FROM ${bd} WHERE CodCliente = ? AND CodAtivo = ? And QtdeAtivo > 0;`;
+  const [ativos] = await connection.execute(query, [CodCliente, codAtivo]);
+  return ativos as IAtivoUsuario[];
+};
 
-// const decrementAtivosUsuarioQtde = async (
-//   codAtivo:number,
-//   CodCliente: number, 
-//   QtdAtivo: number,
-// ): Promise<ResultSetHeader> => {
-//   const q = `UPDATE ${cliente} SET QtdeAtivo = ? WHERE CodAtivo = ? AND CodCliente = ?`;
-//   const [update] = await connection.execute<ResultSetHeader>(q, [QtdAtivo, codAtivo, CodCliente]);
-//   return update;
-// };
+const decrementAtivosUsuarioQtde = async (
+  codAtivo:number,
+  CodCliente: number, 
+  QtdAtivo: number,
+): Promise<ResultSetHeader> => {
+  const q = `UPDATE ${bd} SET QtdeAtivo = QtdeAtivo - ? WHERE CodAtivo = ? AND CodCliente = ?`;
+  const [update] = await connection.execute<ResultSetHeader>(q, [QtdAtivo, codAtivo, CodCliente]);
+  return update;
+};
 
 export default {
   getAtivosUsuarioByCodCliente,
-  // getAtivosUsuarioByCodClienteAndCodAtivo,
-  // decrementAtivosUsuarioQtde,
+  getAtivosUsuarioByCodClienteAndCodAtivo,
+  decrementAtivosUsuarioQtde,
 };
