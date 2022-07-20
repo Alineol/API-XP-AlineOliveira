bbd59cc92f21a9df405fb5b6effb9a08f62711a4
Aline <allinne.oliveira.ol@gmail.com>
2022-07-20 12:17:42 -0300

    ✨ feat(ativoservice.ts): adiciona função sellAtivoCorretora

--- src/services/ativoService.ts
+++ src/services/ativoService.ts
@@ -1,13 +1,14 @@
 import IAtivo from '../interfaces/IAtivo';
 import ativoModel from '../models/ativoModel';
-// import userService from './userService';
+import userService from './userService';
+import helpers from '../helpers';
 
 // import { checkAuthorization } from 
 
 const getAtivosCorretoraByCodAtivo = async (cod:number): Promise<IAtivo | string> => {
   const [ativo] = await ativoModel.getAtivosCorretoraByCodAtivo(cod);
   if (!ativo) {
-    return '';
+    return 'Ativo não encontrado';
   }
   return {
     CodAtivo: ativo.CodAtivo,
@@ -16,19 +17,25 @@ const getAtivosCorretoraByCodAtivo = async (cod:number): Promise<IAtivo | string
   };
 };
 
-// const sale = async (
-//   codAtivo:number,
-//   CodCliente: number, 
-//   QtdAtivo: number,
-//   token: string,
-// ) => {
-//   const authorization = await userService.checkAuthorization(token, CodCliente);
-//   if (!authorization) {
-//     return '';
-//   }
-// };
+const sellAtivosCorretora = async (
+  codAtivo:number,
+  codCliente: number, 
+  qtdeAtivo: number,
+  token: string,
+) => {
+  const authorization = await userService.checkAuthorization(token, codCliente);
+  if (!authorization) return 'Token invalido, sem autorização';
+
+  const [ativoToSell] = await ativoModel.getAtivosCorretoraByCodAtivo(codAtivo);
+  if (!ativoToSell) return 'Ativo não encontrado';
+
+  const checkQtde = helpers.checkAtivosQtdeToDecrement(ativoToSell.QtdeAtivo, qtdeAtivo);
+  if (checkQtde === false) return 'Quantidade de ativos excedida';
+
+  return ativoModel.decrementAtivocCorretoraQtde(codAtivo, qtdeAtivo);
+};
 
 export default {
   getAtivosCorretoraByCodAtivo,
-  // sale,
+  sellAtivosCorretora,
 };
