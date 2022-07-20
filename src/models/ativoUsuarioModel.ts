5c3ce58f5156662102e43a38d9204c90e3832444
Aline <allinne.oliveira.ol@gmail.com>
2022-07-20 16:48:31 -0300

    ✨ feat(ativousuariomodel.ts): adiciona as funções incrementQtdeAtivo e createAtivoUsuario

--- src/models/ativoUsuarioModel.ts
+++ src/models/ativoUsuarioModel.ts
@@ -1,6 +1,7 @@
 import { ResultSetHeader } from 'mysql2';
 import connection from './connection';
 import IAtivoUsuario from '../interfaces/IAtivoUsuario';
+import IAtivo from '../interfaces/IAtivo';
 
 const bd = 'XPCorretora.AtivosUsuarios';
 
@@ -20,6 +21,22 @@ const getAtivosUsuarioByCodClienteAndCodAtivo = async (
   return ativos as IAtivoUsuario[];
 };
 
+const createAtivoUsuario = async (ativo: IAtivo, codCliente: number): Promise<ResultSetHeader> => {
+  const { codAtivo, valor, qtdeAtivo } = ativo;
+  const query = `INSERT INTO ${bd} (codAtivo, qtdeAtivo, valor, codCliente) VALUES (?, ?, ?, ?);`;
+  const [insert] = await connection.execute<ResultSetHeader>(query, [
+    codAtivo, qtdeAtivo, valor, codCliente]);
+  return insert;
+};
+
+const incrementQtdeAtivo = async (ativo: IAtivo, codCliente: number): Promise<ResultSetHeader> => {
+  const { codAtivo, qtdeAtivo } = ativo;
+  console.log(qtdeAtivo);
+  const q = `UPDATE ${bd} SET qtdeAtivo = qtdeAtivo + ? WHERE codCliente = ? AND codAtivo = ?;`;
+  const [update] = await connection.execute<ResultSetHeader>(q, [qtdeAtivo, codCliente, codAtivo]);
+  return update;
+};
+
 const decrementAtivosUsuarioQtde = async (
   codAtivo:number,
   codCliente: number, 
@@ -30,10 +47,10 @@ const decrementAtivosUsuarioQtde = async (
   return update;
 };
 
-// todo cria lógica para aumentar a quantidade de ativos do usuario 
-
 export default {
   getAtivosUsuarioByCodCliente,
   getAtivosUsuarioByCodClienteAndCodAtivo,
   decrementAtivosUsuarioQtde,
+  createAtivoUsuario,
+  incrementQtdeAtivo,
 };
