29ea5e70fc5960169951efbd4831f8acc981e811
Aline <allinne.oliveira.ol@gmail.com>
2022-07-20 12:22:42 -0300

    ✨ feat(contaservice.ts): adiciona constante no lugar de string direta

--- src/services/contaService.ts
+++ src/services/contaService.ts
@@ -3,11 +3,12 @@ import contaModel from '../models/contaModel';
 import userService from './userService';
 import IAccount from '../interfaces/IAccount';
 
+const athorizationMessage = 'Token invalido, sem autorização';
+
 const getByCodCliente = async (cod: number, token: string):Promise<IAccount | string> => {
   const authorization = await userService.checkAuthorization(token, cod);
-  if (!authorization) {
-    return '';
-  }
+  if (!authorization) return athorizationMessage;
+  
   const [account] = await contaModel.getByCodCliente(cod);
   return {
     CodCliente: account.CodCliente,
@@ -18,18 +19,16 @@ const getByCodCliente = async (cod: number, token: string):Promise<IAccount | st
 const getMoney = async (cod:number, value: number, token: string):
 Promise<string | ResultSetHeader> => {
   const authorization = await userService.checkAuthorization(token, cod);
-  if (!authorization) {
-    return '';
-  }
+  if (!authorization) return athorizationMessage;
+
   return contaModel.decrementAccount(cod, value);
 };
 
 const putMoney = async (cod:number, value: number, token: string):
 Promise<string | ResultSetHeader> => {
   const authorization = await userService.checkAuthorization(token, cod);
-  if (!authorization) {
-    return '';
-  }
+  if (!authorization) return athorizationMessage;
+
   return contaModel.increaseAccount(cod, value);
 };
 
