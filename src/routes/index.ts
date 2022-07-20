7a7f24c4e814be61a9cc1416c07cbd3a78ef6a25
Aline <allinne.oliveira.ol@gmail.com>
2022-07-20 10:17:37 -0300

    ✨ feat(routes/index.ts): adiciona rota de investimentos e adiciona mid de token na rota de ativos

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,12 +2,14 @@ import { Router } from 'express';
 import userRoutes from './userRoutes';
 import ativoRoutes from './ativoRoutes';
 import loginRoute from './loginRoute';
+import investimentosRoutes from './investimentosRoutes';
 import userMid from '../middlewares/userMiddlewares';
 
 const route = Router();
 
-route.use('/ativos', ativoRoutes);
+route.use('/ativos', userMid.validatetoken, ativoRoutes);
 route.use('/cliente', userMid.validatetoken, userRoutes);
+route.use('/investimentos', userMid.validatetoken, investimentosRoutes);
 route.use('/login', loginRoute);
 
 export default route;
