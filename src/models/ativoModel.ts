e9248309abdb6074a14f76292e2d24631db62b0d
Aline <allinne.oliveira.ol@gmail.com>
2022-07-20 12:19:15 -0300

    ✨ feat(ativomodel.ts): adiciona função decrementAtivocCorretoraQtde

--- src/models/ativoModel.ts
+++ src/models/ativoModel.ts
@@ -1,5 +1,8 @@
+import { ResultSetHeader } from 'mysql2';
 import connection from './connection';
 import IAtivo from '../interfaces/IAtivo';
+
+const bd = 'XPCorretora.AtivosCorretora';
 
 const getAtivosCorretoraByCodAtivo = async (cod: number): Promise<IAtivo[]> => {
   const query = 'SELECT * FROM XPCorretora.AtivosCorretora WHERE CodAtivo = ?;';
@@ -7,6 +10,14 @@ const getAtivosCorretoraByCodAtivo = async (cod: number): Promise<IAtivo[]> => {
   return ativo as IAtivo[];
 };
 
+const decrementAtivocCorretoraQtde = async (codAtivo:number, qtdAtivo: number)
+: Promise<ResultSetHeader> => {
+  const q = `UPDATE ${bd} SET QtdeAtivo = QtdeAtivo - ? WHERE CodAtivo = ?`;
+  const [update] = await connection.execute<ResultSetHeader>(q, [qtdAtivo, codAtivo]);
+  return update;
+};
+
 export default {
   getAtivosCorretoraByCodAtivo,
+  decrementAtivocCorretoraQtde,
 };
