7b4e135e4fcd97e9b5671752a3a0777f58f9158c
Aline <allinne.oliveira.ol@gmail.com>
2022-07-20 10:13:14 -0300

    ✨ feat(ativousuariocontroller.ts): adiciona função sellAtivosUsuarios

--- src/controllers/ativoUsuarioController.ts
+++ src/controllers/ativoUsuarioController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import ativoUsuarioService from '../services/ativoUsuarioService';
+import helpers from '../helpers';
 
 const getAtivosUsuarioByCodCliente = async (req: Request, res: Response): Promise<Response> => {
   const { CodCliente } = req.params;
@@ -10,6 +11,20 @@ const getAtivosUsuarioByCodCliente = async (req: Request, res: Response): Promis
   return res.status(200).json(ativos);
 };
 
+const sellAtivosUsuarios = async (req: Request, res: Response): Promise<Response> => {
+  const { codCliente, codAtivo, qtdeAtivo } = req.body;
+  const { authorization } = req.headers;
+  const sell = await ativoUsuarioService.sellAtivosUsuarios(
+    codAtivo, 
+    codCliente,
+    qtdeAtivo,
+    authorization as string,
+  );
+  const response = helpers.validateResponse(sell);
+  return res.status(response.code).json({ message: response.message });
+};
+
 export default {
   getAtivosUsuarioByCodCliente,
+  sellAtivosUsuarios,
 };
