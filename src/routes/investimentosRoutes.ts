6fc322be51a1e8ab74c24613cbc17602f650f0e4
Aline <allinne.oliveira.ol@gmail.com>
2022-07-20 12:20:17 -0300

    ✨ feat(investimentosrouter.ts): adiciona rota post./investimentos/comprar

--- src/routes/investimentosRoutes.ts
+++ src/routes/investimentosRoutes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import ativoUsuarioController from '../controllers/ativoUsuarioController';
+import ativoController from '../controllers/ativoController';
 import investimentosMiddleware from '../middlewares/investimentosMiddleware';
 
 const route = Router();
@@ -8,6 +9,12 @@ route.post(
   '/vender', 
   investimentosMiddleware.validateBodyInvestimentos,
   ativoUsuarioController.sellAtivosUsuarios,
+);
+
+route.post(
+  '/comprar', 
+  investimentosMiddleware.validateBodyInvestimentos,
+  ativoController.sellAtivoCorretora,
 );
 
 export default route;
