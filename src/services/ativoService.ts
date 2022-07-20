a82b8356acb122831ec4e45f6cfa5b8b02993fbf
Aline <allinne.oliveira.ol@gmail.com>
2022-07-20 16:50:00 -0300

    ✨ feat(ativoservice.ts): cria função updateAtivoUsuario
    função responsável por atualizar a tabela de ativoUsuario se acordo com o ativo comprado

--- src/services/ativoService.ts
+++ src/services/ativoService.ts
@@ -2,8 +2,7 @@ import IAtivo from '../interfaces/IAtivo';
 import ativoModel from '../models/ativoModel';
 import userService from './userService';
 import helpers from '../helpers';
-
-// import { checkAuthorization } from 
+import ativoUsuarioModel from '../models/ativoUsuarioModel';
 
 const getAtivosCorretoraByCodAtivo = async (cod:number): Promise<IAtivo | string> => {
   const [ativo] = await ativoModel.getAtivosCorretoraByCodAtivo(cod);
@@ -15,6 +14,15 @@ const getAtivosCorretoraByCodAtivo = async (cod:number): Promise<IAtivo | string
     qtdeAtivo: ativo.qtdeAtivo,
     valor: Number(ativo.valor),
   };
+};
+
+const updateAtivoUsuario = (ativo: IAtivo, codCliente: number):void => {
+  const { codAtivo } = ativo;
+  ativoUsuarioModel.getAtivosUsuarioByCodClienteAndCodAtivo(codAtivo, codCliente).then((result) => {
+    if (result.length === 0) { 
+      ativoUsuarioModel.createAtivoUsuario(ativo, codCliente);
+    } else { ativoUsuarioModel.incrementQtdeAtivo(ativo, codCliente); }
+  });
 };
 
 const sellAtivosCorretora = async (
@@ -31,7 +39,8 @@ const sellAtivosCorretora = async (
 
   const checkQtde = helpers.checkAtivosQtdeToDecrement(ativoToSell.qtdeAtivo, qtdeAtivo);
   if (checkQtde === false) return 'Quantidade de ativos excedida';
-  // todo cria lógica para aumentar a quantidade de ativos do cliente após uma compra
+  // ! lógica para aumentar a quantidade de ativos do cliente após uma compra
+  updateAtivoUsuario({ codAtivo, qtdeAtivo, valor: ativoToSell.valor }, codCliente);
   return ativoModel.decrementAtivocCorretoraQtde(codAtivo, qtdeAtivo);
 };
 
