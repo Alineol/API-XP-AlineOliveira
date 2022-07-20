a3d2bc3a63e30f1f75309aebeb267752475b919a
Aline <allinne.oliveira.ol@gmail.com>
2022-07-20 10:16:54 -0300

    ♻️ refactor(ativoroutes.ts): remove mid de validação de token

--- src/routes/ativoRoutes.ts
+++ src/routes/ativoRoutes.ts
@@ -1,13 +1,11 @@
 import { Router } from 'express';
 import ativoController from '../controllers/ativoController';
-import userMid from '../middlewares/userMiddlewares';
 import mid from '../middlewares/ativoMiddleware';
 
 const route = Router();
 
 route.get(
   '/:CodAtivo', 
-  userMid.validatetoken,
   mid.validateAtivosCorretoraParams, 
   ativoController.getAtivosCorretoraByCodAtivo,
 );
