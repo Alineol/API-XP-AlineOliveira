8e9ac5034223b9cae402122b30e37af8c06d862c
Aline <allinne.oliveira.ol@gmail.com>
2022-07-20 10:12:39 -0300

    ✨ feat(ativousuarioservice.ts): adiciona função sellAtivosUsuarios

--- src/services/ativoUsuarioService.ts
+++ src/services/ativoUsuarioService.ts
@@ -1,6 +1,8 @@
+import { ResultSetHeader } from 'mysql2';
 import IAtivoUsuario from '../interfaces/IAtivoUsuario';
 import ativoUsuarioModel from '../models/ativoUsuarioModel';
 import userService from './userService';
+import helpers from '../helpers';
 
 const getAtivosUsuarioByCodCliente = async (cod: number, token: string)
 : Promise<IAtivoUsuario[] | string> => {
@@ -17,6 +19,29 @@ const getAtivosUsuarioByCodCliente = async (cod: number, token: string)
   }));
 };
 
+const sellAtivosUsuarios = async (
+  codAtivo: number,
+  codCliente:number, 
+  qtdeAtivo: number, 
+  token: string,
+): Promise<ResultSetHeader | string > => {
+  const authorization = await userService.checkAuthorization(token, codCliente);
+  if (!authorization) {
+    // TODO mudar esse retorno para frase
+    return '';
+  }
+  const [ativoToSell] = await ativoUsuarioModel
+    .getAtivosUsuarioByCodClienteAndCodAtivo(codAtivo, codCliente);
+  if (!ativoToSell) return 'Ativo não encontrado';
+  const checkQtde = helpers.checkAtivosQtdeToDecrement(ativoToSell.QtdeAtivo, qtdeAtivo);
+  if (checkQtde === false) {
+    return 'Quantidade de ativos excedida';
+  }
+  return ativoUsuarioModel.decrementAtivosUsuarioQtde(codAtivo, codCliente, qtdeAtivo);
+  // eu ia apagar a quantidade total mas decidi só zerar para manter o registro
+};
+
 export default {
   getAtivosUsuarioByCodCliente,
+  sellAtivosUsuarios,
 };
