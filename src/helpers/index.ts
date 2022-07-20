63dfce9bbecedade477bc9694cc93ef1fb69a888
Aline <allinne.oliveira.ol@gmail.com>
2022-07-20 12:21:19 -0300

    ♻️ refactor(helpers/index.ts): altera um dos cases

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -2,15 +2,12 @@ import { ResultSetHeader } from 'mysql2';
 
 function validateResponse(retono: string | ResultSetHeader) {
   switch (retono) {
-    case '':
-      return { code: 401, message: 'Token invalido, sem autorização' };
-      // todo mudar a check authorization nas services
     case 'Token invalido, sem autorização':
-      return { code: 400, message: retono };
+      return { code: 401, message: retono };
     case 'Quantidade de ativos excedida':
       return { code: 400, message: retono };
     case 'Ativo não encontrado': 
-      return { code: 400, message: retono };
+      return { code: 404, message: retono };
     default: return { code: 200, message: 'Ação efetuada com sucesso!' };
   }
 }
