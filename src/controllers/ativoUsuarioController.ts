cd3b76827ba258f86179b468cb694008487e0aa7
Aline <allinne.oliveira.ol@gmail.com>
2022-07-20 12:23:53 -0300

    ♻️ refactor(ativousuariocontroller.ts): muda vefiricação na linha 10

--- src/controllers/ativoUsuarioController.ts
+++ src/controllers/ativoUsuarioController.ts
@@ -7,7 +7,9 @@ const getAtivosUsuarioByCodCliente = async (req: Request, res: Response): Promis
   const { authorization } = req.headers;
   const ativos = await ativoUsuarioService
     .getAtivosUsuarioByCodCliente(Number(CodCliente), authorization as string);
-  if (ativos === '') return res.status(401).json({ message: 'Token invalido, sem autorização' }); 
+  if (ativos === 'Token invalido, sem autorização') {
+    return res.status(401).json({ message: ativos });
+  } 
   return res.status(200).json(ativos);
 };
 
