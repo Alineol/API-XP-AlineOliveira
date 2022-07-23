618e8e15e4874f8bffdd1a415d65e9f6884f628b
Aline <allinne.oliveira.ol@gmail.com>
2022-07-22 21:11:12 -0300

    🧪 test: adiciona testes na camada service de ativos

--- src/test/unit/services/01-ativoService.spec.js
+++ src/test/unit/services/01-ativoService.spec.js
@@ -5,20 +5,22 @@ const { expect } = require('chai');
 const ativoModel = require('../../../models/ativoModel');
 const ativoService = require('../../../services/ativoService');
 const userService = require('../../../services/userService');
-// const ativoUsuarioModel = require('../../../models/ativoUsuarioModel');
-const { ativo } = require('../helpers');
+const { ativo, arrayVazio, arrayDeAtivos } = require('../helpers');
+const jwt = require('../../../jwt/index');
 
 describe('Ao pegar ativos da corretora por codAtivo(service):', () => {
   describe('- Quando o ativo não existe:', () => {
     before(() => {
       sinon.stub(ativoModel, 'pegaAtivosCorretoraPorCodAtivo').resolves('');
+      sinon.stub(jwt, 'conferirToken').resolves(true);
     });
     after(() => {
       ativoModel.pegaAtivosCorretoraPorCodAtivo.restore();
+      jwt.conferirToken.restore();
     });
 
     it('retorna uma string com a frase "Ativo não encontrado".', async () => {
-      const response = await ativoService.pegaAtivosCorretoraPorCodAtivo(1);
+      const response = await ativoService.pegaAtivosCorretoraPorCodAtivo(1, 'sgfsgggghshshh');
       expect(response).to.be.an('string');
       expect(response).to.be.equal('Ativo não encontrado');
     });
@@ -26,15 +28,32 @@ describe('Ao pegar ativos da corretora por codAtivo(service):', () => {
   describe('- Quando o ativo existe:', () => {
     before(() => {
       sinon.stub(ativoModel, 'pegaAtivosCorretoraPorCodAtivo').resolves(ativo);
+      sinon.stub(jwt, 'conferirToken').resolves(true);
     });
     after(() => {
       ativoModel.pegaAtivosCorretoraPorCodAtivo.restore();
+      jwt.conferirToken.restore();
     });
 
     it('retorna um objeto não vazio.', async () => {
-      const response = await ativoService.pegaAtivosCorretoraPorCodAtivo(1);
+      const response = await ativoService.pegaAtivosCorretoraPorCodAtivo(1, 'sgsgsggsgsgs');
       expect(response).to.be.an('object');
       expect(response).not.to.be.empty;
+    });
+  });
+  describe('- Quando o usuario não está logado:', () => {
+    before(() => {
+      sinon.stub(ativoModel, 'pegaAtivosCorretoraPorCodAtivo').resolves(ativo);
+      sinon.stub(jwt, 'conferirToken').resolves(false);
+    });
+    after(() => {
+      ativoModel.pegaAtivosCorretoraPorCodAtivo.restore();
+      jwt.conferirToken.restore();
+    });
+
+    it('retorna uma string com a frase "Token invalido, sem autorização".', async () => {
+      const response = await ativoService.pegaAtivosCorretoraPorCodAtivo(1, 'sgsgsggsgsgs');
+      expect(response).to.be.equal('Token invalido, sem autorização');
     });
   });
 });
@@ -101,6 +120,54 @@ describe('Ao tentar realizar a venda de um ativo(service):', () => {
     it('retorna uma string com a frase: "ok"', async () => {
       const response = await ativoService.sellAtivosCorretora(1, 1, 2, 'akjdhqodiadjadja');
       expect(response).to.be.equal('ok');
+    });
+  });
+});
+
+describe('Ao tentar pegar todos os ativos disponíveis no BD(service)', () => {
+  describe('-Se não houver ativos no BD', () => {
+    before(() => {
+      sinon.stub(ativoModel, 'pegarTodosOsAtivosCorretora').resolves(arrayVazio[0]);
+      sinon.stub(jwt, 'conferirToken').resolves(true);
+    });
+    after(() => {
+      ativoModel.pegarTodosOsAtivosCorretora.restore();
+      jwt.conferirToken.restore();
+    });
+    it('retorna uma string com a frase "Não há ativos disponíveis".', async () => {
+      const response = await ativoService.pegarTodosOsAtivosCorretora();
+      expect(response).to.be.equal('Não há ativos disponíveis');
+    });
+  });
+
+  describe('-Se houver houver ativos no BD', () => {
+    before(() => {
+      sinon.stub(ativoModel, 'pegarTodosOsAtivosCorretora').resolves(arrayDeAtivos[0]);
+      sinon.stub(jwt, 'conferirToken').resolves(true);
+    });
+    after(() => {
+      ativoModel.pegarTodosOsAtivosCorretora.restore();
+      jwt.conferirToken.restore();
+    });
+    it('retorna um array de objetos não vazios.', async () => {
+      const response = await ativoService.pegarTodosOsAtivosCorretora('dsaffgaggagagtr');
+      expect(response).to.be.an('array');
+      expect(response[0]).to.be.an('object');
+      expect(response[1]).not.to.be.empty;
+    });
+  });
+  describe('-Se o usuario não estiver logado', () => {
+    before(() => {
+      sinon.stub(ativoModel, 'pegarTodosOsAtivosCorretora').resolves(arrayDeAtivos[0]);
+      sinon.stub(jwt, 'conferirToken').resolves(false);
+    });
+    after(() => {
+      ativoModel.pegarTodosOsAtivosCorretora.restore();
+      jwt.conferirToken.restore();
+    });
+    it('retorna uma string com a frase "Token invalido, sem autorização".', async () => {
+      const response = await ativoService.pegarTodosOsAtivosCorretora('dsaffgaggagagtr');
+      expect(response).to.be.equal('Token invalido, sem autorização');
     });
   });
 });
