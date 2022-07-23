b944a43f7c7e41b483f1733707684098fe1a2435
Aline <allinne.oliveira.ol@gmail.com>
2022-07-23 18:41:01 -0300

    🐛 fix(app.js): remove option de shwagger

--- src/app.js
+++ src/app.js
@@ -5,14 +5,11 @@ const routes = require('./routes');
 const swaggerConfig = require('./docs/swagger.json');
 
 const swaggerDoc = swaggerJsdoc(swaggerConfig);
-const options = {
-  explorer: true,
-};
 
 const app = express();
 
 app.use(express.json());
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc, options));
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 app.use('/', routes);
 
 module.exports = app;
