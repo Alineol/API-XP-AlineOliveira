deca03d2b65dc37da42d92dca043c2f12c54d2fc
Aline <allinne.oliveira.ol@gmail.com>
2022-07-23 10:44:51 -0300

    ✨ feat: configurações iniciais para o swagger

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 require('dotenv').config();
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 const app = require('./app');
+const swaggerConfig = require('./docs/swagger.config');
 
 const { PORT } = process.env;
+
+const swaggerDoc = swaggerJsdoc(swaggerConfig);
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 const server = app.listen(PORT, () => console.log(
   `Server is running on PORT: ${PORT}`,
