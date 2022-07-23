23ed3fcab4d5ff2f190da9df6048d21ba3e18ef4
Aline <allinne.oliveira.ol@gmail.com>
2022-07-23 20:45:23 -0300

    atualiando query

--- src/models/ativoUsuarioModel.js
+++ src/models/ativoUsuarioModel.js
@@ -1,9 +1,7 @@
 const connection = require('./connection');
 
-const bd = 'XPCorretora.AtivosUsuarios';
-
 const pegarAtivosUsuarioPorCodCliente = async (codCliente) => {
-  const query = 'SELECT * FROM XPCorretora.AtivosUsuarios WHERE codCliente = ? AND qtdeAtivo > 0;';
+  const query = 'SELECT * FROM AtivosUsuarios WHERE codCliente = ? AND qtdeAtivo > 0;';
   const [ativos] = await connection.execute(query, [codCliente]);
   return ativos;
 };
@@ -12,14 +10,14 @@ const pegarAtivosUsuarioPorCodClienteAndCodAtivo = async (
   codAtivo,
   codCliente,
 ) => {
-  const query = `SELECT * FROM ${bd} WHERE codCliente = ? AND codAtivo = ? And qtdeAtivo > 0;`;
+  const query = 'SELECT * FROM AtivosUsuarios WHERE codCliente = ? AND codAtivo = ? And qtdeAtivo > 0;';
   const [ativos] = await connection.execute(query, [codCliente, codAtivo]);
   return ativos;
 };
 
 const criarAtivoUsuario = async (ativo, codCliente) => {
   const { codAtivo, valor, qtdeAtivo } = ativo;
-  const query = `INSERT INTO ${bd} (codAtivo, qtdeAtivo, valor, codCliente) VALUES (?, ?, ?, ?);`;
+  const query = 'INSERT INTO AtivosUsuarios(codAtivo, qtdeAtivo, valor, codCliente) VALUES (?, ?, ?, ?);';
   const [insert] = await connection.execute(query, [
     codAtivo, qtdeAtivo, valor, codCliente]);
   return insert;
@@ -27,7 +25,7 @@ const criarAtivoUsuario = async (ativo, codCliente) => {
 
 const incrementarQtdeAtivo = async (ativo, codCliente) => {
   const { codAtivo, qtdeAtivo } = ativo;
-  const q = `UPDATE ${bd} SET qtdeAtivo = qtdeAtivo + ? WHERE codCliente = ? AND codAtivo = ?;`;
+  const q = 'UPDATE AtivosUsuarios SET qtdeAtivo = qtdeAtivo + ? WHERE codCliente = ? AND codAtivo = ?;';
   const [update] = await connection.execute(q, [qtdeAtivo, codCliente, codAtivo]);
   return update;
 };
@@ -37,7 +35,7 @@ const decrementarAtivosUsuarioQtde = async (
   codCliente,
   qtdeAtivo,
 ) => {
-  const q = `UPDATE ${bd} SET qtdeAtivo = qtdeAtivo - ? WHERE codAtivo = ? AND codCliente = ?`;
+  const q = 'UPDATE AtivosUsuarios SET qtdeAtivo = qtdeAtivo - ? WHERE codAtivo = ? AND codCliente = ?';
   const [update] = await connection.execute(q, [qtdeAtivo, codAtivo, codCliente]);
   return update;
 };
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,14 +1,14 @@
 const connection = require('./connection');
 
 const login = async (email, senha) => {
-  const q = 'SELECT email FROM XPCorretora.Usuarios WHERE email = ? AND senha = ?';
+  const q = 'SELECT email FROM Usuarios WHERE email = ? AND senha = ?';
   const [user] = await connection.execute(q, [email, senha]);
 
   return user;
 };
 
 const pegarUsuarioPorEmail = async (email) => {
-  const q = 'SELECT codCliente FROM XPCorretora.Usuarios WHERE email = ?';
+  const q = 'SELECT codCliente FROM Usuarios WHERE email = ?';
   const [user] = await connection.execute(q, [email]);
   return user;
 };
