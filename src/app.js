f2a98d93d5549e4986269e9f6586071c4cf0405e
Aline <allinne.oliveira.ol@gmail.com>
2022-07-23 16:13:21 -0300

    🐛 fix: altera localização do swagger do index para o app

--- src/app.js
+++ src/app.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 const routes = require('./routes');
+const swaggerConfig = require('./docs/swagger.json');
+
+const swaggerDoc = swaggerJsdoc(swaggerConfig);
+const options = {
+  explorer: true,
+};
 
 const app = express();
 
 app.use(express.json());
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc, options));
 app.use('/', routes);
 
 module.exports = app;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
 require('dotenv').config();
-const swaggerJsdoc = require('swagger-jsdoc');
-const swaggerUi = require('swagger-ui-express');
 const app = require('./app');
-const swaggerConfig = require('./docs/swagger.config.json');
 
 const { PORT } = process.env;
-
-const swaggerDoc = swaggerJsdoc(swaggerConfig);
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 const server = app.listen(PORT, () => console.log(
   `Server is running on PORT: ${PORT}`,
