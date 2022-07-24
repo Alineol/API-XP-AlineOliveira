a54d6edbb2802a66a6ba8b593220a77a5e625343
Aline <allinne.oliveira.ol@gmail.com>
2022-07-24 19:10:16 -0300

    ✨ feat: l

--- src/routes/index.js
+++ src/routes/index.js
@@ -13,5 +13,6 @@ route.use('/cliente', userMid.validatetoken, clienteRoutes);
 route.use('/investimentos', userMid.validatetoken, investimentosRoutes);
 route.use('/login', loginRoute);
 route.use('/admin', adminRoutes);
+route.use('/', (req, res) => res.send('<h1>Olá gente, sejam muito bem vindos à minha api, alguns endpoints precisam de token e body, você pode acessar ela também pelo <a href="https://app.swaggerhub.com/apis-docs/Alineol/xp_api1/1.0.0"> Swagger  </a></h1> '));
 
 module.exports = route;
